f1f0e2bee9761f319f635351236e3daad04fcca6
Cesar Ibarra <cfi2103@columbia.edu>
2016-10-08 04:22:59 -0400

    camera works on physical iphone now

---
+++ ImageCompiler/ImageCompiler/CameraViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CameraViewController.swift
+//  ImageCompiler
+//
+//  Created by Cesar Ibarra on 10/8/16.
+//  Copyright © 2016 ADI House. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func openCamera(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = false
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

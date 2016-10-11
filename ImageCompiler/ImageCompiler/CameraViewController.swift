af96efc7fbbc3b0f56f80eb0a78440b9819912fc
Cesar Ibarra <cfi2103@columbia.edu>
2016-10-10 21:03:20 -0400

    added constants file with oath and api key

--- ImageCompiler/ImageCompiler/CameraViewController.swift
+++ ImageCompiler/ImageCompiler/CameraViewController.swift
@@ -26,7 +26,6 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate,UI
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
---
+++ ImageCompiler/ImageCompiler/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  ImageCompiler
+//
+//  Created by Cesar Ibarra on 10/10/16.
+//  Copyright © 2016 ADI House. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    //Here in case we want to make OAuth requests with a users personal email account
+    struct OAuth {
+        static let clientID = "875572462024-jl6st6ep1np14o3ckk4bh6vmbc4g2a29.apps.googleusercontent.com"
+    }
+    
+    //with the api key we have access to google services without user personal data
+    struct APIKey {
+        static let apiKey = "AIzaSyBN-KYItCdUS3RH7HL-PTnTnGSw4zZscdk"
+    }
+}
